a8cf91a2609f912b1f2fa01dd90a6f5feb7a5d1d
刘无语 <9109223101@email.ncu.edu.cn>
2025-09-12 15:23:37 +0800

    修改学习界面，改完先回答再展示例句

--- 单词/word_learning.sql
+++ 单词/word_learning.sql
@@ -14,6 +14,10 @@
  Date: 10/09/2025 20:55:05
 */
 
+CREATE DATABASE IF NOT EXISTS `word_learning` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+USE `word_learning`;
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
